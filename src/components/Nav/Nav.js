aec45ddf633d1a1a52f80fa9c9a067bebb7b3e88
wzhaoxuan <newzxuan213@gmail.com>
2025-03-16 23:01:01 +0800

    structure file and create navbar component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import './Nav.css';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/skills">Skills</Link>
+        </li>
+        <li>
+          <Link to="/">Portfolio.</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
